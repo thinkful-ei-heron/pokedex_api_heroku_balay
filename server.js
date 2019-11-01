29ce2b4d2b11be833cba17b7315dfd4f05daa94b
balayaydemir <balayaydemir@gmail.com>
2019-11-01 08:22:40 -0700

    changes for deployment

--- server.js
+++ server.js
@@ -1,18 +1,75 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const POKEDEX = require('./pokedex.json');
+
+
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
+
+app.use(function validateBearerToken(req, res, next) {
+  
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+    
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+    // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+  
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+  
+  res.json(response);
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+ 
 });
